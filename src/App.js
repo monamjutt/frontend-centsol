2fe69d1256e736a6e9ac13d08a8a8fe65c7cee50
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2019-12-23 11:35:44 +0500

    React's functional and class components basic

--- src/App.js
+++ src/App.js
@@ -1,74 +1,16 @@
-import React, { useState, Fragment } from 'react'
-import AddUserForm from './forms/AddUserForm'
-import EditUserForm from './forms/EditUserForm'
-import UserTable from './tables/UserTable'
+import React, { Component } from 'react';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
-const App = () => {
-	// Data
-	const usersData = [
-
-	]
-
-	const initialFormState = { id: null, name: '', username: '' }
-
-	// Setting state
-	const [ users, setUsers ] = useState(usersData)
-	const [ currentUser, setCurrentUser ] = useState(initialFormState)
-	const [ editing, setEditing ] = useState(false)
-
-	// CRUD operations
-	const addUser = user => {
-		user.id = users.length + 1
-		setUsers([ ...users, user ])
-	}
-
-	const deleteUser = id => {
-		setEditing(false)
-
-		setUsers(users.filter(user => user.id !== id))
-	}
-
-	const updateUser = (id, updatedUser) => {
-		setEditing(false)
-
-		setUsers(users.map(user => (user.id === id ? updatedUser : user)))
-	}
-
-	const editRow = user => {
-		setEditing(true)
-
-		setCurrentUser({ id: user.id, name: user.name, username: user.username })
-	}
-
-	return (
-		<div className="container">
-			<h1>CRUD App with Hooks</h1>
-			<div className="flex-row">
-				<div className="flex-large">
-					{editing ? (
-						<Fragment>
-							<h2>Edit user</h2>
-							<EditUserForm
-								editing={editing}
-								setEditing={setEditing}
-								currentUser={currentUser}
-								updateUser={updateUser}
-							/>
-						</Fragment>
-					) : (
-						<Fragment>
-							<h2>Add user</h2>
-							<AddUserForm addUser={addUser} />
-						</Fragment>
-					)}
-				</div>
-				<div className="flex-large">
-					<h2>View users</h2>
-					<UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
-				</div>
+class App extends Component {
+	render(){
+		return(
+			<div className="App">
+				<Greet></Greet>
+				<Welcome />
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default App
