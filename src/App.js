8b818e5ef4cbedca35b39181e913154a508532e5
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2019-12-27 11:24:48 +0500

    JSX example added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 class App extends Component {
 	render(){
 		return(
 			<div className="App">
-				<Greet></Greet>
-				<Welcome />
+				<Hello />
 			</div>
 		)
 	}
