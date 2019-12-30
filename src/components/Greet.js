8fec1dfdadbb8fb2407e83fe7f6c7b00f3160b6e
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2019-12-30 18:05:28 +0500

    How to destruct props

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -3,13 +3,16 @@ import React from 'react';
 // function Greet(){
 //   return <h1>Hello, jutt</h1>
 // }
-
-const Greet = (props) => {
-
+// desturcting props means
+// extract props values into separate variables
+// one way of doing it that pass {variables...} instead of props to functional component
+// 2nd way of doing it extract them in function body itself.
+const Greet = ({name, heroName, children}) => {
+  // const {name, heroName, children} = props;
   return (
     <div>
-      <h1>Hello {props.name} aka {props.heroName}</h1>
-      {props.children}
+      <h1>Hello {name} aka {heroName}</h1>
+      {children}
     </div>
   )
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 
+// desturcting props means
+// extract props values into separate variables
+// 2nd way of doing it extract them in render function body.
 class Welcome extends Component{
   render(){
+    const {name, heroName} = this.props;
     return (
       <div>
-        <h1>Hello {this.props.name} aka {this.props.heroName}</h1>
+        <h1>Hello {name} aka {heroName}</h1>
         {this.props.children}
       </div>
     )
-  return <h1>Hello {this.props.name} aka {this.props.heroName}</h1>
   }
 }
 
