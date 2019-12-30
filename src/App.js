3957e2fb7cb78ef96e6837cca691efa5255a8a0a
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2019-12-30 18:40:11 +0500

    Event handling code added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import Count from './components/Count';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 class App extends Component {
 	render(){
 		return(
 			<div className="App">
-				<Count></Count>
+				<FunctionClick></FunctionClick>
+				<ClassClick></ClassClick>
 			</div>
 		)
 	}
