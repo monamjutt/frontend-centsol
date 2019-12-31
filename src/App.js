b62d49a048513adf94f47ac619514abee7b5c78c
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2019-12-31 14:45:40 +0500

    List rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
 import './App.css'
 
 class App extends Component {
 	render(){
 		return(
 			<div className="App">
-				<UserGreeting></UserGreeting>
+				<NameList></NameList>
 			</div>
 		)
 	}
---
+++ src/components/NameList.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Person from './Person'
+
+
+function NameList() {
+  const names = ['Bruce', 'Clark', 'Diana'];
+  // return (
+  //   // <div>
+  //   //   <h2>{names[0]}</h2>
+  //   //   <h2>{names[1]}</h2>
+  //   //   <h2>{names[2]}</h2>
+  //   // </div>
+  //   <div>
+  //     {
+  //       names.map(name => <h2>{name}</h2>)
+  //     }
+  //   </div>
+  // )
+  // const nameList = names.map(name => <h2>{name}</h2>)
+  // return(
+  //   <div>
+  //     {
+  //       nameList
+  //     }
+  //   </div>
+  // )
+
+  const persons = [
+    {
+      name: 'Bruce',
+      age: 31,
+      heroName: 'Batman'
+    },
+    {
+      name: 'Clark',
+      age: 30,
+      heroName: 'Superman'
+    },
+    {
+      name: 'Diana',
+      age: 35,
+      heroName: 'Wonder Woman'
+    }
+  ];
+
+  const personList = persons.map( person => <Person person={person}/> )
+
+  return  <div> { personList } </div>
+  
+}
+
+export default NameList
