492be422749547deebecf7b2cb8013a41aef5c7c
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2019-12-31 11:24:51 +0500

    Event binding in react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+import './App.css'
 
 class App extends Component {
 	render(){
 		return(
 			<div className="App">
-				<FunctionClick></FunctionClick>
-				<ClassClick></ClassClick>
+				<EventBind></EventBind>
 			</div>
 		)
 	}
