f30b831ec2dc42ca52c3418d10d9559e42497572
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2019-12-31 13:20:14 +0500

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
 import './App.css'
 
 class App extends Component {
 	render(){
 		return(
 			<div className="App">
-				<ParentComponent></ParentComponent>
+				<UserGreeting></UserGreeting>
 			</div>
 		)
 	}
